f9c33aa9eaaed35aea1c3edcd26d87f6caf6a2f8
kamu <kamuxxx@hotmail.com>
2018-02-05 21:29:36 +0900

    first commit

---
+++ Sample/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections.ObjectModel;
+
+namespace Sample.ViewModels
+{
+	public class MainPageViewModel : BindableBase, INavigationAware
+	{
+        public ObservableCollection<string> ItemsSource { get; set; } 
+
+		public MainPageViewModel()
+		{
+            ItemsSource = new ObservableCollection<string>(new List<string>{
+                "A","B","C","D","E"
+            });
+		}
+
+		public void OnNavigatedFrom(NavigationParameters parameters)
+		{
+
+		}
+
+		public void OnNavigatedTo(NavigationParameters parameters)
+		{
+			
+		}
+
+		public void OnNavigatingTo(NavigationParameters parameters)
+		{
+		}
+	}
+}
+
